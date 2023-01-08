25b594114fb4eb054d5b84847d1722824ac8ddfd
yakuhito <y@kuhi.to>
2023-01-08 08:40:23 -0800

    got server running

--- main.go
+++ main.go
@@ -1,16 +1,48 @@
 package main
 
 import (
-	"context"
-	"flag"
+	"os"
 	"fmt"
 	"log"
 	"net"
-
-	"google.golang.org/grpc"
+	"context"
 	pb "golden-gate/grpc"
+	"google.golang.org/grpc"
 )
 
-func (s *server) SayHelloAgain(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
-        return &pb.HelloReply{Message: "Hello again " + in.GetName()}, nil
+type server struct {
+	pb.UnimplementedGoldenGateServer
+}
+
+func (s *server) RefreshAPIKeyData(context.Context, *pb.RefreshAPIKeyRequest) (*pb.RefreshAPIKeyReply, error) {
+	return &pb.RefreshAPIKeyReply{
+		CanBeUsed: true,
+	}, nil
+}
+
+func gRPCServer(port string) {
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%s", port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	pb.RegisterGoldenGateServer(s, &server{})
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
+
+func getPort() string {
+   port := os.Getenv("GOLDEN_GATE_LISTEN_PORT")
+   if port == "" {
+       panic("GOLDEN_GATE_LISTEN_PORT not set.")
+   }
+
+   return port
+}
+
+func main() {
+	port := getPort()
+	gRPCServer(port)
 }
