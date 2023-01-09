e122bce62e1b2d299b545dcd30a9998a7dd63bbe
yakuhito <y@kuhi.to>
2023-01-09 01:11:29 -0800

    revamp models.go

--- models.go
+++ models.go
@@ -4,18 +4,25 @@ import (
     "database/sql"
 )
 
-type User struct {
+type UserInfo struct {
     RemainingCredits            int64          `json:"remaining_credits"`
     StripeCustomerID            sql.NullString `json:"stripe_customer_id"`
     AutoPurchaseCreditsPackages bool           `json:"auto_purchase_credits_packages"`
 }
 
-type APIKey struct {
+type APIKeyInfo struct {
     Disabled           bool   `json:"disabled"`
     MonthlyCreditLimit int64  `json:"monthly_credit_limit"`
     Origin             string `json:"origin"`
 }
 
-type Usage struct {
+type UsageInfo struct {
     Credits int64 `json:"credits"`
+}
+
+type DataDudeResponse struct {
+    Success    bool       `json:"success"`
+    APIKey     APIKeyInfo `json:"api_key"`
+    User       UserInfo   `json:"api_key"`
+    Usage      UsageInfo  `json:"api_key"`
 }
